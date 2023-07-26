a7e83f02cb4e95c14e5e40454d7fc64ccf2444bf
AndreyT <zanarions@gmail.com>
2023-07-26 22:14:04 +0300

    Fix #1

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -10,7 +10,6 @@ public class BonusServiceTest {
         long amount = 1000;
         boolean registered = true;
         long expected = 30;
-
         long actual = service.calculate(amount, registered);
 
         Assertions.assertEquals(expected, actual);
